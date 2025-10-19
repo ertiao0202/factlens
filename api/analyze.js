e5450b5ae271324c76086036b49deffa837b9dcc
ertiao0202 <worldweekly@gmail.com>
2025-10-19 20:25:22 +0800

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,4 +1,4 @@
-/export default async function handler(req) {
+export default async function handler(req) {
   if (req.method !== 'POST') {
     return new Response(JSON.stringify({ error: 'Method Not Allowed' }),
       { status: 405, headers: { 'Content-Type': 'application/json' } });
